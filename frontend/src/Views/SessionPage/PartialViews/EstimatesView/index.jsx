5d2f8e1473207ea20b912a48d20b93da9a00150d
Eslam Metawie <eslam.metawie@dos-online.de>
2024-11-13 22:19:26 +0100

    current session wurde zurückgestellt

--- frontend/src/Views/SessionPage/PartialViews/EstimatesView/index.jsx
+++ frontend/src/Views/SessionPage/PartialViews/EstimatesView/index.jsx
@@ -3,6 +3,7 @@ import FrameComponent from "../../../../Components/Frames/FrameComponent";
 import { Divider, IconButton, Stack } from "@mui/material";
 import { AddCircleOutline } from "@mui/icons-material";
 import StoryComp from "./Components/StoryComp";
+import RoundComp from "./Components/RoundComp";
 import EstimateForm from "./Components/EstimateForm";
 import EstimatesFooter from "./Components/EstimatesFooter";
 import useStories from "../../../../Common/Hooks/useStories";
@@ -17,8 +18,12 @@ const EstimatesView = () => {
             paperSx={{
                 flex: 1,
             }}
-            sx={{ paddingY: 1, paddingX: 0 }}
-            title={"Stories"}
+            sx={{
+                paddingY: 1,
+                paddingX: 0,
+                mt: 2, // زيادة المسافة العلوية
+            }}
+            title={"Estimates"}
             icon={
                 <IconButton>
                     <AddCircleOutline color="secondary" fontSize="small" />
@@ -29,29 +34,26 @@ const EstimatesView = () => {
                 <LoaderComp />
             ) : (
                 <>
-                    <Stack flex={1} spacing={1} paddingX={1} justifyContent="space-between">
-                        {/* قسم الكروت */}
-                        <Stack spacing={1} flex={1} overflow={"auto"}>
-                            {stories.map((story) => (
-                                <StoryComp
-                                    key={story?.id}
-                                    title={story?.title}
-                                    selected={selectedStory.id === story?.id}
-                                    estimate={story?.estimate}
-                                    onClick={() => handleSelectStory(story?.id)}
-                                />
-                            ))}
-                        </Stack>
-
-                        {/* الفاصل */}
-                        <Divider sx={{ my: 1 }} />
-
-                        <Stack spacing={1}>
+                    <Stack spacing={1} height={"25%"} overflow={"auto"} paddingX={1}>
+                        {stories.map((story) => (
+                            <StoryComp
+                                key={story?.id}
+                                title={story?.title}
+                                selected={selectedStory.id === story?.id}
+                                estimate={story?.estimate}
+                                onClick={() => handleSelectStory(story?.id)}
+                            />
+                        ))}
+                    </Stack>
+                    <Stack height={"65%"} spacing={2} px={2} mt={2}>
+                        <Divider />
+                        <RoundComp title="Current round:" time="01:27" onClick={() => {}} />
+                        <Divider />
+                        <Stack justifyContent={"space-between"} flex={1}>
                             <EstimateForm />
                             <EstimatesFooter sessionId={"a23389cc"} />
                         </Stack>
                     </Stack>
-
                 </>
             )}
         </FrameComponent>
