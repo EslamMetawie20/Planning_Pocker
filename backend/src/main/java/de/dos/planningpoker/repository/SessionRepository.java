2230cb3a6e1f34b3e7061b84f9194db54a2c87e6
zijian Ying <zijian.ying@dos-online.de>
2024-11-27 21:12:39 +0100

    SessionRepository erweitern

--- backend/src/main/java/de/dos/planningpoker/repository/SessionRepository.java
+++ backend/src/main/java/de/dos/planningpoker/repository/SessionRepository.java
@@ -1,9 +1,12 @@
 package de.dos.planningpoker.repository;
 
-import de.dos.planningpoker.model.Session;
+import de.dos.planningpoker.model.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
+    Optional<Session> findBySessionCode(String sessionCode);
 }
