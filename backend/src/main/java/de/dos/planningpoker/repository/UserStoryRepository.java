d45f11a488636004922d5ad1e26eb2f73837b2b7
zijian Ying <zijian.ying@dos-online.de>
2024-11-30 19:23:32 +0100

    UserStoryRepository mit neu Query einfügen

--- backend/src/main/java/de/dos/planningpoker/repository/UserStoryRepository.java
+++ backend/src/main/java/de/dos/planningpoker/repository/UserStoryRepository.java
@@ -4,6 +4,9 @@ import de.dos.planningpoker.model.entity.UserStory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserStoryRepository extends JpaRepository<UserStory, Long> {
+    Optional<UserStory> findByUserStoryCode(String userStoryCode);
 }
