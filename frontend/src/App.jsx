46eac6adc3ba9b784a59b31a0722eccef5987c5f
SuhibAlsaggar <sohib.saggar90@gmail.com>
2024-12-03 14:35:41 +0100

    Frontend - fixed merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useRoutes, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useRoutes } from "react-router-dom";
 import BackgroundBox from "./Components/Frames/BackgroundBox";
 import SessionPage from "./Views/SessionPage";
 import MainSession from "./Views/StartPage/MainSession";
@@ -9,7 +8,6 @@ import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import useWebSocketListener from "./Common/Hooks/useWebSocketListener.jsx";
 import "./App.css";
-import { getTokenData } from "./Common/Utils/tokenUtils.js";
 
 const App = () => {
   useWebSocketListener();
@@ -24,15 +22,13 @@ const App = () => {
 export default App;
 
 const AppRoutes = () => {
-  const sessionId = useSelector((state) => state.session.sessionId);
-
   const MainRoutes = {
     path: "/",
     element: <BackgroundBox />,
     children: [
       {
         path: "",
-        element: <SessionPage />,
+        element: <MainSession />,
       },
       {
         path: "session",
