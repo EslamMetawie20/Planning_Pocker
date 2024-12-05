e027304b69a9325e96799dffa07672cc00c8f41b
SuhibAlsaggar <sohib.saggar90@gmail.com>
2024-12-05 19:48:09 +0100

    Frontend - added default story

---
+++ frontend/src/Common/Vars/DefaultData.js
@@ -0,0 +1,4 @@
+export const DEFAULT_STORY = {
+  title: "Example Story",
+  content: `<p><strong>Beschreibung:</strong></p><p>Example description</p><p><br></p><p><strong>Ist - Zustand:</strong></p><p>Example state</p><p><br></p><p><strong>Soll - Zustand:</strong></p><p>Example target</p><p><br></p><p><strong style="background-color: initial;">AKZ</strong><strong>:</strong></p><ul><li>Example 1</li><li>Example 2</li><li>Example 3</li></ul><p><br></p><p><strong>Fazit:</strong></p><p>Example conclusion</p>`,
+};
