3cda25101888281daa6f9128e39314848078496f
Eslam Metawie <eslammetawie50@gmail.com>
2024-12-09 20:56:00 +0100

    ConfirmationDialog was edited

--- frontend/src/Views/SessionPage/PartialViews/EstimatesView/Components/EstimatesFooter.jsx
+++ frontend/src/Views/SessionPage/PartialViews/EstimatesView/Components/EstimatesFooter.jsx
@@ -12,6 +12,9 @@ import {
 } from "@mui/material";
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import QuilEditor from "../../../../../PartialViews/QuilEditor.jsx";
+import ConfirmationDialog from "../../../../../PartialViews/ConfirmationDialog";
+
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
@@ -60,84 +63,14 @@ const EstimatesFooter = ({
                 {buttonLabel}
             </Button>
 
-            <Dialog
+            <ConfirmationDialog
                 open={open}
-                TransitionComponent={Transition}
-                keepMounted
                 onClose={handleClose}
-                aria-describedby="alert-dialog-slide-description"
-            >
-                <DialogTitle
-                    sx={{
-                        backgroundColor: "#004259",
-                        color: "white",
-                        fontWeight: "bold",
-                        fontFamily: "'Source Sans Pro', sans-serif",
-                        padding: "1rem 1rem",
-                    }}
-                >
-                    Are you sure?
-                </DialogTitle>
-                <DialogContent
-                    sx={{
-                        padding: "1.5rem 1.5rem 0.5rem",
-                        marginTop: "1rem",
-                    }}
-                >
-                    <DialogContentText
-                        id="alert-dialog-slide-description"
-                        sx={{
-                            fontFamily: "'Source Sans Pro', sans-serif",
-                            fontSize: "1rem",
-                            color: "#333",
-                        }}
-                    >
-                        Do you really want to end the session? This action cannot be undone.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions
-                    sx={{
-                        justifyContent: "center",
-                        padding: "1rem",
-                    }}
-                >
-                    <Button
-                        onClick={handleClose}
-                        sx={{
-                            backgroundColor: "white",
-                            color: "red",
-                            fontFamily: "'Source Sans Pro', sans-serif",
-                            padding: "0.5rem 2rem",
-                            borderRadius: "8px",
-                            fontWeight: "bold",
-                            border: "2px solid red",
-                            "&:hover": {
-                                backgroundColor: "#ffe6e6",
-                            },
-                        }}
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        onClick={handleConfirm}
-                        sx={{
-                            backgroundColor: "white",
-                            color: "#CAD400",
-                            fontFamily: "'Source Sans Pro', sans-serif",
-                            padding: "0.5rem 2rem",
-                            borderRadius: "8px",
-                            fontWeight: "bold",
-                            border: "2px solid #CAD400",
-                            "&:hover": {
-                                backgroundColor: "#f9f9f9",
-                            },
-                        }}
-                    >
-                        Confirm
-                    </Button>
-                </DialogActions>
-
-            </Dialog>
+                onConfirm={handleConfirm}
+                title="Are you sure?"
+                description="Do you really want to end the session? This action cannot be undone."
+                confirmColor="#CAD400"
+            />
         </Stack>
     );
 };
