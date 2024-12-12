f7403cb950e3c81a4a525fcdd7762c279a0954fc
Eslam Metawie <eslammetawie50@gmail.com>
2024-12-12 22:52:23 +0100

    Implemented delete confirmation dialog functionality

--- frontend/src/Views/SessionPage/PartialViews/EstimatesView/Components/StoryComp.jsx
+++ frontend/src/Views/SessionPage/PartialViews/EstimatesView/Components/StoryComp.jsx
@@ -1,44 +1,32 @@
 import {
-  BookmarkAdded,
-  BookmarkBorderOutlined,
-  DoNotDisturbOnOutlined,
+    BookmarkAdded,
+    BookmarkBorderOutlined,
+    DoNotDisturbOnOutlined,
 } from "@mui/icons-material";
 import {
-  IconButton,
-  Stack,
-  Typography,
-  useTheme,
+    IconButton,
+    Stack,
+    Typography,
+    useTheme,
 } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import ConfirmationDialog from "../../../../../PartialViews/ConfirmationDialog";
 
 const StoryComp = ({
-                     title = "Example Story Title",
-                     selected = false,
-                     estimate = 0,
-                     disabled = false,
-                     onClick = () => {},
-                     onDelete = () => {},
+                       title = "Example Story Title",
+                       selected = false,
+                       estimate = 0,
+                       disabled = false,
+                       onClick = () => {},
+                       onDelete = () => {},
                    }) => {
-  const [open, setOpen] = useState(false);
-  const theme = useTheme();
+    const theme = useTheme();
 
-  const handleDelete = () => {
-    setOpen(true);
-  };
-
-  const handleConfirmDelete = () => {
-    setOpen(false);
-    onDelete();
-  };
-
-  return (
-      <>
+    return (
         <Stack
             border={selected ? 2 : 1}
             borderColor={
-              selected ? theme.palette.secondary.main : theme.palette.grey[300]
+                selected ? theme.palette.secondary.main : theme.palette.grey[300]
             }
             width={"100%"}
             borderRadius={2}
@@ -49,77 +37,69 @@ const StoryComp = ({
             alignItems={"center"}
             onClick={!disabled ? onClick : null}
             sx={{
-              cursor: !disabled && "pointer",
-              transition: "all 0.2s ease",
-              "&:hover": !disabled && {
-                backgroundColor: theme.palette.action.hover,
-                transform: "scale(1.02)",
-              },
+                cursor: !disabled && "pointer",
+                transition: "all 0.2s ease",
+                "&:hover": !disabled && {
+                    backgroundColor: theme.palette.action.hover,
+                    transform: "scale(1.02)",
+                },
             }}
         >
-          {!disabled && selected && estimate === 0 && (
-              <IconButton
-                  sx={{
-                    padding: "0",
-                    height: "fit-content",
-                    width: "fit-content",
-                    minWidth: "unset",
-                  }}
-                  onClick={handleDelete}
-              >
-                <DoNotDisturbOnOutlined sx={{ height: "1rem", width: "1rem" }} />
-              </IconButton>
-          )}
-          <Stack
-              direction={"row"}
-              spacing={1}
-              alignItems={"center"}
-              width={"100%"}
-          >
-            {estimate !== 0 ? (
-                <BookmarkAdded color="secondary" />
-            ) : (
-                <BookmarkBorderOutlined color="secondary" />
+            {!disabled && selected && estimate === 0 && (
+                <IconButton
+                    sx={{
+                        padding: "0",
+                        height: "fit-content",
+                        width: "fit-content",
+                        minWidth: "unset",
+                    }}
+                    onClick={onDelete}
+                >
+                    <DoNotDisturbOnOutlined sx={{ height: "1rem", width: "1rem" }} />
+                </IconButton>
             )}
-            <Typography
-                variant="h6"
-                sx={{
-                  overflow: "hidden",
-                  whiteSpace: "nowrap",
-                  textOverflow: "ellipsis",
-                  maxWidth: "10rem",
-                }}
+            <Stack
+                direction={"row"}
+                spacing={1}
+                alignItems={"center"}
+                width={"100%"}
             >
-              {title}
+                {estimate !== 0 ? (
+                    <BookmarkAdded color="secondary" />
+                ) : (
+                    <BookmarkBorderOutlined color="secondary" />
+                )}
+                <Typography
+                    variant="h6"
+                    sx={{
+                        overflow: "hidden",
+                        whiteSpace: "nowrap",
+                        textOverflow: "ellipsis",
+                        maxWidth: "10rem",
+                    }}
+                >
+                    {title}
+                </Typography>
+            </Stack>
+            <Typography
+                width={"10%"}
+                variant="h4"
+                fontWeight={"400"}
+                color={estimate !== 0 ? "secondary" : "primary"}
+            >
+                {estimate === 0 ? "--" : estimate}
             </Typography>
-          </Stack>
-          <Typography
-              width={"10%"}
-              variant="h4"
-              fontWeight={"400"}
-              color={estimate !== 0 ? "secondary" : "primary"}
-          >
-            {estimate === 0 ? "--" : estimate}
-          </Typography>
         </Stack>
-        <ConfirmationDialog
-            open={open}
-            onClose={() => setOpen(false)}
-            onConfirm={handleConfirmDelete}
-            title="Are you sure?"
-            description="Do you really want to delete this story? This action cannot be undone."
-        />
-      </>
-  );
+    );
 };
 
 StoryComp.propTypes = {
-  title: PropTypes.string.isRequired,
-  selected: PropTypes.bool,
-  estimate: PropTypes.number,
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func,
-  onDelete: PropTypes.func,
+    title: PropTypes.string.isRequired,
+    selected: PropTypes.bool,
+    estimate: PropTypes.number,
+    disabled: PropTypes.bool,
+    onClick: PropTypes.func,
+    onDelete: PropTypes.func,
 };
 
 export default StoryComp;
--- frontend/src/Views/SessionPage/PartialViews/EstimatesView/index.jsx
+++ frontend/src/Views/SessionPage/PartialViews/EstimatesView/index.jsx
@@ -10,13 +10,13 @@ import EstimatesFooter from "./Components/EstimatesFooter";
 import useStories from "../../../../Common/Hooks/useStories";
 import LoaderComp from "../../../../Components/Extras/LoaderComp";
 import QuilEditor from "../../../../PartialViews/QuilEditor.jsx";
+import ConfirmationDialog from "../../../../PartialViews/ConfirmationDialog";
 import { useDispatch, useSelector } from "react-redux";
 import {
   endSession,
   leaveSession,
 } from "../../../../_redux/reducers/sessionSlice.js";
 import { revealVotes } from "../../../../_redux/reducers/storySlice.js";
-import DeleteConfirmationDialog from "./Components/DeleteConfirmationDialog.jsx";
 
 const EstimatesView = () => {
   const {
@@ -27,20 +27,22 @@ const EstimatesView = () => {
     handleDeleteStory,
   } = useStories();
   const [openQuilEditor, setOpenQuilEditor] = useState(false);
+  const [openDelete, setOpenDelete] = useState(false);
   const dispatch = useDispatch();
   const { sessionId, isScrumMaster, roundStart, roundEnd } = useSelector(
-    (state) => state.session
+      (state) => state.session
   );
 
   const addStory = (title, content) => {
     handleAddStory(title, content);
     setOpenQuilEditor(false);
   };
+
   const messages = {
     endSession:
-      "Are you sure you want to end the session? This action cannot be undone and may affect other participants.",
+        "Are you sure you want to end the session? This action cannot be undone and may affect other participants.",
     leaveSession:
-      "Are you sure you want to leave the session? You can rejoin later if needed, but some changes may not be saved.",
+        "Are you sure you want to leave the session? You can rejoin later if needed, but some changes may not be saved.",
   };
 
   const handleRevealVotes = () => {
@@ -55,7 +57,6 @@ const EstimatesView = () => {
     dispatch(endSession());
   };
 
-  const [openDelete, setOpenDelete] = useState(false);
   const handleDeleteClick = () => {
     setOpenDelete(true);
   };
@@ -80,119 +81,104 @@ const EstimatesView = () => {
   }, [isScrumMaster, stories.length]);
 
   return (
-    <>
-      <FrameComponent
-        paperSx={{
-          flex: 1,
-        }}
-        sx={{
-          paddingY: 1,
-          paddingX: 0,
-        }}
-        title={"User stories"}
-        icon={
-          isScrumMaster && (
-            <IconButton
-              onClick={() => setOpenQuilEditor(true)}
-              className={blinking ? "blinking" : ""}
-            >
-              <AddCircleOutline color="secondary" fontSize="small" />
-            </IconButton>
-          )
-        }
-      >
-        {false ? (
-          <LoaderComp />
-        ) : (
-          <Stack height={"100%"}>
-            <Stack spacing={1} height={"25%"} overflow={"auto"} paddingX={1}>
-              {stories.map((story) => (
-                <StoryComp
-                  key={story?.id}
-                  title={story?.title}
-                  selected={selectedStory?.id === story?.id}
-                  estimate={story?.estimate}
-                  disabled={!isScrumMaster}
-                  onDelete={handleDeleteClick}
-                  onClick={() => handleSelectStory(story?.id)}
-                />
-              ))}
-            </Stack>
-            <Box px={2}>
-              <Divider />
-            </Box>
-            <Box px={2} my={2}>
-              <CurrentVotes
-                startTime={roundStart}
-                endTime={roundEnd}
-                title="Current votes:"
-                onClick={handleRevealVotes}
-              />
-            </Box>
-            <Box px={2}>
-              <Divider />
-            </Box>
-            <Stack flex={1} justifyContent={"space-between"} px={2} mt={2}>
-              {isScrumMaster && <EstimateForm />}
-              <Box display={"flex"} flexDirection={"column"} marginTop={"auto"}>
-                <EstimatesFooter
-                  sessionId={sessionId}
-                  buttonLabel={isScrumMaster ? "End Session" : "Leave Session"}
-                  confirmationMessage={
-                    isScrumMaster ? messages.endSession : messages.leaveSession
-                  }
-                  onClick={
-                    isScrumMaster ? handleEndSession : handleLeaveSession
-                  }
-                />
-              </Box>
-            </Stack>
-          </Stack>
-        )}
-      </FrameComponent>
+      <>
+        <FrameComponent
+            paperSx={{
+              flex: 1,
+            }}
+            sx={{
+              paddingY: 1,
+              paddingX: 0,
+            }}
+            title={"User stories"}
+            icon={
+                isScrumMaster && (
+                    <IconButton
+                        onClick={() => setOpenQuilEditor(true)}
+                        className={blinking ? "blinking" : ""}
+                    >
+                      <AddCircleOutline color="secondary" fontSize="small" />
+                    </IconButton>
+                )
+            }
+        >
+          {false ? (
+              <LoaderComp />
+          ) : (
+              <Stack height={"100%"}>
+                <Stack spacing={1} height={"25%"} overflow={"auto"} paddingX={1}>
+                  {stories.map((story) => (
+                      <StoryComp
+                          key={story?.id}
+                          title={story?.title}
+                          selected={selectedStory?.id === story?.id}
+                          estimate={story?.estimate}
+                          disabled={!isScrumMaster}
+                          onDelete={handleDeleteClick}
+                          onClick={() => handleSelectStory(story?.id)}
+                      />
+                  ))}
+                </Stack>
+                <Box px={2}>
+                  <Divider />
+                </Box>
+                <Box px={2} my={2}>
+                  <CurrentVotes
+                      startTime={roundStart}
+                      endTime={roundEnd}
+                      title="Current votes:"
+                      onClick={handleRevealVotes}
+                  />
+                </Box>
+                <Box px={2}>
+                  <Divider />
+                </Box>
+                <Stack flex={1} justifyContent={"space-between"} px={2} mt={2}>
+                  {isScrumMaster && <EstimateForm />}
+                  <Box display={"flex"} flexDirection={"column"} marginTop={"auto"}>
+                    <EstimatesFooter
+                        sessionId={sessionId}
+                        buttonLabel={isScrumMaster ? "End Session" : "Leave Session"}
+                        confirmationMessage={
+                          isScrumMaster ? messages.endSession : messages.leaveSession
+                        }
+                        onClick={
+                          isScrumMaster ? handleEndSession : handleLeaveSession
+                        }
+                    />
+                  </Box>
+                </Stack>
+              </Stack>
+          )}
+        </FrameComponent>
 
-      <Dialog
-        open={openQuilEditor}
-        onClose={() => setOpenQuilEditor(false)}
-        maxWidth="md"
-        fullWidth
-      >
-        <QuilEditor
-          sendData={addStory}
-          onSubmit={() => setOpenQuilEditor(false)}
-          initial={{
-            title: "",
-            content:
-              "<b>Beschreibung: </b> <br /> <br />  <b>Ist - Zustand:</b> <br /> <br /> <b>Soll- Zustand:</b> <br /> <br />  <b>AKZ: </b> <br /> <br /> <b>Fazit:</b> <br /> <br />",
-          }}
-          buttonLabel="Save"
+        <ConfirmationDialog
+            open={openDelete}
+            onClose={() => handleCloseDelete()}
+            onConfirm={deleteStory}
+            title="Confirm Deletion"
+            description={`Are you sure you want to delete ${selectedStory?.title || "this story"}? This action cannot be undone.`}
         />
-      </Dialog>
-      <DeleteConfirmationDialog
-        open={openDelete}
-        onClose={() => handleCloseDelete()}
-        onConfirm={deleteStory}
-        itemName={selectedStory?.title}
-      />
-    </>
+
+        <Dialog
+            open={openQuilEditor}
+            onClose={() => setOpenQuilEditor(false)}
+            maxWidth="md"
+            fullWidth
+        >
+          <QuilEditor
+              sendData={addStory}
+              onSubmit={() => setOpenQuilEditor(false)}
+              initial={{
+                title: "",
+                content:
+                    "<b>Beschreibung: </b> <br /> <br />  <b>Ist - Zustand:</b> <br /> <br /> <b>Soll- Zustand:</b> <br /> <br />  <b>AKZ: </b> <br /> <br /> <b>Fazit:</b> <br /> <br />",
+              }}
+              buttonLabel="Save"
+          />
+        </Dialog>
+      </>
   );
 };
 
 export default EstimatesView;
-
-const styles = `
-@keyframes blinkingEffect {
-    0% { opacity: 1; }
-    50% { opacity: 0; }
-    100% { opacity: 1; }
-}
-
-.blinking {
-    animation: blinkingEffect 1.5s infinite;
-}
-`;
-
-const styleSheet = document.createElement("style");
-styleSheet.type = "text/css";
-styleSheet.innerText = styles;
-document.head.appendChild(styleSheet);
